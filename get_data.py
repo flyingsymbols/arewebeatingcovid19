322e5d3f3e8c0ee7aa39b130b6dfff27af031b3b
flyingsymbols <david.hogarty@gmail.com>
2020-04-29 21:10:00 -0400

    updated api url, data from 2020-04-29

--- get_data.py
+++ get_data.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-r = requests.get('https://covidtracking.com/api/states/daily')
+r = requests.get('https://covidtracking.com/api/v1/states/daily.json')
 json_obj = json.loads(r.content)
 
 with open('data.json', 'w') as f:
